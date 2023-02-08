b31d48b806ad3ddd99c2d4b306794440cb111e4b
Jacob Vanderhorst <jacob@ad.oldran.ch>
2023-02-07 23:41:15 -0800

    base

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="h-screen flex flex-col justify-center content-center">
+        <div class="mx-auto">
+            <h1 class="text-xl">Welcome!</h1>
+            <ul>
+                <a href="https://media.oldran.ch:8920"><li>Jellyfin</li></a>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+a { @apply text-blue-700 font-bold }
+</style>
